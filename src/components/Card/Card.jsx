f54373eb05171640d86fe887753859d25d0f87ff
Anushka Sharma <s.anushka0804@gmail.com>
2019-10-03 00:04:56 +0530

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-// nodejs library that concatenates classes
-import classNames from "classnames";
-// nodejs library to set properties for components
-import PropTypes from "prop-types";
-// @material-ui/core components
-import withStyles from "@material-ui/core/styles/withStyles";
-// @material-ui/icons
 
-// core components
+import classNames from "classnames";
+
+import PropTypes from "prop-types";
+
+import withStyles from "@material-ui/core/styles/withStyles";
+
+
+
 import cardStyle from "assets/jss/material-kit-react/components/cardStyle.jsx";
 
 function Card({ ...props }) {
