0db3fa2738bda296f46fe2cb742c126d534d7874
Anushka Sharma <s.anushka0804@gmail.com>
2019-10-03 00:03:35 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-// nodejs library that concatenates classes
+
 import classNames from "classnames";
-// nodejs library to set properties for components
+
 import PropTypes from "prop-types";
-// @material-ui/core components
+
 import withStyles from "@material-ui/core/styles/withStyles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -11,9 +11,9 @@ import IconButton from "@material-ui/core/IconButton";
 import Button from "@material-ui/core/Button";
 import Hidden from "@material-ui/core/Hidden";
 import Drawer from "@material-ui/core/Drawer";
-// @material-ui/icons
+
 import Menu from "@material-ui/icons/Menu";
-// core components
+
 import headerStyle from "assets/jss/material-kit-react/components/headerStyle.jsx";
 
 class Header extends React.Component {
